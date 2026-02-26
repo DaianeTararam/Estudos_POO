918c00c112fd2763febb7ceb169c935905b78da2
DaianeTararam <daalkmim@gmail.com>
2026-02-26 12:00:35 -0300

    Primeiro commit

---
+++ src/model/EstouroStack.java
@@ -0,0 +1,12 @@
+package model;
+
+public class EstouroStack {
+    public static void main(String[] args) {
+        System.out.println("Segue em andamento o estouro da stack");
+        estourando();
+    }
+
+    public static void estourando(){
+        estourando();
+    }
+}
